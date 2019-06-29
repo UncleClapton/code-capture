e764cab48c3788b6ebffbbdcb3ee92667b020ee0
Cameron Welter <camjwelt@gmail.com>
2019-06-29 22:24:21 +0000

    modernize code a bit.

--- src/extension.js
+++ src/extension.js
@@ -1,4 +1,4 @@
-const vscode = require('vscode')
+const vscode = require('vscode') /* eslint-disable-line import/no-unresolved */// this is fine
 const fs = require('fs')
 const path = require('path')
 const { homedir } = require('os')
@@ -10,36 +10,124 @@ const writeSerializedBlobToFile = (serializeBlob, fileName) => {
 
 const P_TITLE = 'Polacode 📸'
 
+
+
+const getHtmlContent = (htmlPath) => {
+  const htmlContent = fs.readFileSync(htmlPath, 'utf-8')
+  return htmlContent.replace(/script src="([^"]*)"/gu, (match, src) => {
+    const realSource = `vscode-resource:${path.resolve(htmlPath, '..', src)}`
+    return `script src="${realSource}"`
+  })
+}
+
+
+
+
+
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+const activate = (context) => {
   const htmlPath = path.resolve(context.extensionPath, 'webview/index.html')
 
   let lastUsedImageUri = vscode.Uri.file(path.resolve(homedir(), 'Desktop/code.png'))
-  let panel
+  let panel = null
+
+
+
+
+
+  const syncSettings = () => {
+    const settings = vscode.workspace.getConfiguration('polacode')
+    const editorSettings = vscode.workspace.getConfiguration('editor', null)
+    panel.webview.postMessage({
+      type: 'updateSettings',
+      shadow: settings.get('shadow'),
+      transparentBackground: settings.get('transparentBackground'),
+      backgroundColor: settings.get('backgroundColor'),
+      target: settings.get('target'),
+      ligature: editorSettings.get('fontLigatures'),
+    })
+  }
+
+  const setupMessageListeners = () => {
+    panel.webview.onDidReceiveMessage(({ type, data }) => {
+      switch (type) {
+        case 'shoot':
+          vscode.window
+            .showSaveDialog({
+              defaultUri: lastUsedImageUri,
+              filters: {
+                Images: ['png'],
+              },
+            })
+            .then((uri) => {
+              if (uri) {
+                writeSerializedBlobToFile(data.serializedBlob, uri.fsPath)
+                lastUsedImageUri = uri
+              }
+            })
+          break
+
+        case 'getAndUpdateCacheAndSettings':
+          panel.webview.postMessage({
+            type: 'restoreBgColor',
+            bgColor: context.globalState.get('polacode.bgColor', '#2e3440'),
+          })
+
+          syncSettings()
+          break
+
+        case 'updateBgColor':
+          context.globalState.update('polacode.bgColor', data.bgColor)
+          break
+
+        case 'invalidPasteContent':
+          vscode.window.showInformationMessage(
+            'Pasted content is invalid. Only copy from VS Code and check if your shortcuts for copy/paste have conflicts.'
+          )
+          break
+
+        default:
+          break
+      }
+    })
+  }
+
+  const setupSelectionSync = () => vscode.window.onDidChangeTextEditorSelection((event) => {
+    if (event.selections[0] && !event.selections[0].isEmpty) {
+      vscode.commands.executeCommand('editor.action.clipboardCopyAction')
+      panel.postMessage({
+        type: 'update',
+      })
+    }
+  })
+
+
+
+
 
   vscode.window.registerWebviewPanelSerializer('polacode', {
-    async deserializeWebviewPanel(_panel, state) {
+    deserializeWebviewPanel: (_panel, state) => {
       panel = _panel
       panel.webview.html = getHtmlContent(htmlPath)
       panel.webview.postMessage({
         type: 'restore',
         innerHTML: state.innerHTML,
-        bgColor: context.globalState.get('polacode.bgColor', '#2e3440')
+        bgColor: context.globalState.get('polacode.bgColor', '#2e3440'),
       })
       const selectionListener = setupSelectionSync()
       panel.onDidDispose(() => {
         selectionListener.dispose()
       })
       setupMessageListeners()
-    }
+    },
   })
 
   vscode.commands.registerCommand('polacode.activate', () => {
     panel = vscode.window.createWebviewPanel('polacode', P_TITLE, 2, {
       enableScripts: true,
-      localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'webview'))]
+      localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'webview'))],
     })
 
     panel.webview.html = getHtmlContent(htmlPath)
@@ -51,91 +139,21 @@ function activate(context) {
 
     setupMessageListeners()
 
-    const fontFamily = vscode.workspace.getConfiguration('editor').fontFamily
+    const { fontFamily } = vscode.workspace.getConfiguration('editor')
     const bgColor = context.globalState.get('polacode.bgColor', '#2e3440')
     panel.webview.postMessage({
       type: 'init',
       fontFamily,
-      bgColor
+      bgColor,
     })
 
     syncSettings()
   })
 
-  vscode.workspace.onDidChangeConfiguration(e => {
-    if (e.affectsConfiguration('polacode') || e.affectsConfiguration('editor')) {
+  vscode.workspace.onDidChangeConfiguration((event) => {
+    if (event.affectsConfiguration('polacode') || event.affectsConfiguration('editor')) {
       syncSettings()
     }
-  })
-
-  function setupMessageListeners() {
-    panel.webview.onDidReceiveMessage(({ type, data }) => {
-      switch (type) {
-        case 'shoot':
-          vscode.window
-            .showSaveDialog({
-              defaultUri: lastUsedImageUri,
-              filters: {
-                Images: ['png']
-              }
-            })
-            .then(uri => {
-              if (uri) {
-                writeSerializedBlobToFile(data.serializedBlob, uri.fsPath)
-                lastUsedImageUri = uri
-              }
-            })
-          break
-        case 'getAndUpdateCacheAndSettings':
-          panel.webview.postMessage({
-            type: 'restoreBgColor',
-            bgColor: context.globalState.get('polacode.bgColor', '#2e3440')
-          })
-
-          syncSettings()
-          break
-        case 'updateBgColor':
-          context.globalState.update('polacode.bgColor', data.bgColor)
-          break
-        case 'invalidPasteContent':
-          vscode.window.showInformationMessage(
-            'Pasted content is invalid. Only copy from VS Code and check if your shortcuts for copy/paste have conflicts.'
-          )
-          break
-      }
-    })
-  }
-
-  function syncSettings() {
-    const settings = vscode.workspace.getConfiguration('polacode')
-    const editorSettings = vscode.workspace.getConfiguration('editor', null)
-    panel.webview.postMessage({
-      type: 'updateSettings',
-      shadow: settings.get('shadow'),
-      transparentBackground: settings.get('transparentBackground'),
-      backgroundColor: settings.get('backgroundColor'),
-      target: settings.get('target'),
-      ligature: editorSettings.get('fontLigatures')
-    })
-  }
-
-  function setupSelectionSync() {
-    return vscode.window.onDidChangeTextEditorSelection(e => {
-      if (e.selections[0] && !e.selections[0].isEmpty) {
-        vscode.commands.executeCommand('editor.action.clipboardCopyAction')
-        panel.postMessage({
-          type: 'update'
-        })
-      }
-    })
-  }
-}
-
-function getHtmlContent(htmlPath) {
-  const htmlContent = fs.readFileSync(htmlPath, 'utf-8')
-  return htmlContent.replace(/script src="([^"]*)"/g, (match, src) => {
-    const realSource = 'vscode-resource:' + path.resolve(htmlPath, '..', src)
-    return `script src="${realSource}"`
   })
 }
 
--- webview/index.js
+++ webview/index.js
@@ -1,4 +1,16 @@
-;(function() {
+/* global acquireVsCodeApi, domtoimage, Vivus */
+/* eslint-disable no-bitwise, no-magic-numbers */
+
+const getRgba = (hex, transparentBackground) => {
+  const bigint = parseInt(hex.slice(1), 16)
+  const red = (bigint >> 16) & 255
+  const green = (bigint >> 8) & 255
+  const blue = bigint & 255
+  const alpha = transparentBackground ? 0 : 1
+  return `rgba(${red}, ${green}, ${blue}, ${alpha})`
+}
+
+const activate = () => {
   const vscode = acquireVsCodeApi()
 
   let target = 'container'
@@ -6,7 +18,7 @@
   let backgroundColor = '#f2f2f2'
 
   vscode.postMessage({
-    type: 'getAndUpdateCacheAndSettings'
+    type: 'getAndUpdateCacheAndSettings',
   })
 
   const snippetNode = document.getElementById('snippet')
@@ -14,14 +26,15 @@
   const obturateur = document.getElementById('save')
 
   snippetContainerNode.style.opacity = '1'
-  const oldState = vscode.getState();
+  const oldState = vscode.getState()
   if (oldState && oldState.innerHTML) {
     snippetNode.innerHTML = oldState.innerHTML
   }
 
-  const getInitialHtml = fontFamily => {
+  const getInitialHtml = (fontFamily) => {
     const cameraWithFlashEmoji = String.fromCodePoint(128248)
     const monoFontStack = `${fontFamily},SFMono-Regular,Consolas,DejaVu Sans Mono,Ubuntu Mono,Liberation Mono,Menlo,Courier,monospace`
+    // eslint-disable-next-line max-len
     return `<meta charset="utf-8"><div style="color: #d8dee9;background-color: #2e3440; font-family: ${monoFontStack};font-weight: normal;font-size: 12px;line-height: 18px;white-space: pre;"><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">0. Run command \`Polacode ${cameraWithFlashEmoji}\`</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">1. Copy some code</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">2. Paste into Polacode view</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">3. Click the button ${cameraWithFlashEmoji}</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div></div></div>`
   }
 
@@ -32,39 +45,35 @@
       const bytes = new Uint8Array(fileReader.result)
       cb(Array.from(bytes).join(','))
     }
-    function getBrightness(color) {
-      const rgb = this.toRgb()
-      return (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000
-    }
 
     fileReader.readAsArrayBuffer(blob)
   }
 
-  function shoot(serializedBlob) {
+  const shoot = (serializedBlob) => {
     vscode.postMessage({
       type: 'shoot',
       data: {
-        serializedBlob
-      }
+        serializedBlob,
+      },
     })
   }
 
-  function getBrightness(hexColor) {
+  const getBrightness = (hexColor) => {
     const rgb = parseInt(hexColor.slice(1), 16)
-    const r = (rgb >> 16) & 0xff
-    const g = (rgb >> 8) & 0xff
-    const b = (rgb >> 0) & 0xff
-    return (r * 299 + g * 587 + b * 114) / 1000
-  }
-  function isDark(hexColor) {
-    return getBrightness(hexColor) < 128
-  }
-  function getSnippetBgColor(html) {
-    const match = html.match(/background-color: (#[a-fA-F0-9]+)/)
-    return match ? match[1] : undefined;
+    const red = (rgb >> 16) & 0xff
+    const green = (rgb >> 8) & 0xff
+    const blue = (rgb >> 0) & 0xff
+    return (red * 299 + green * 587 + blue * 114) / 1000
   }
 
-  function updateEnvironment(snippetBgColor) {
+  const isDark = (hexColor) => getBrightness(hexColor) < 128
+
+  const getSnippetBgColor = (html) => {
+    const match = html.match(/background-color: (#[a-fA-F0-9]+)/u)
+    return match ? match[1] : undefined
+  }
+
+  const updateEnvironment = (snippetBgColor) => {
     // update snippet bg color
     document.getElementById('snippet').style.backgroundColor = snippetBgColor
 
@@ -76,35 +85,40 @@
     }
   }
 
-  function getMinIndent(code) {
+  const getMinIndent = (code) => {
     const arr = code.split('\n')
 
     let minIndentCount = Number.MAX_VALUE
-    for (let i = 0; i < arr.length; i++) {
-      const wsCount = arr[i].search(/\S/)
+
+    arr.forEach((line) => {
+      const wsCount = line.search(/\S/u)
       if (wsCount !== -1) {
         if (wsCount < minIndentCount) {
           minIndentCount = wsCount
         }
       }
-    }
+    })
 
     return minIndentCount
   }
 
-  function stripInitialIndent(html, indent) {
+  const stripInitialIndent = (html, indent) => {
     const doc = new DOMParser().parseFromString(html, 'text/html')
+
     const initialSpans = doc.querySelectorAll('div > div span:first-child')
-    for (let i = 0; i < initialSpans.length; i++) {
-      initialSpans[i].textContent = initialSpans[i].textContent.slice(indent)
+
+
+    for (let index = 0; index < initialSpans.length; index += 1) {
+      initialSpans[index].textContent = initialSpans[index].textContent.slice(indent)
     }
+
     return doc.body.innerHTML
   }
 
-  document.addEventListener('paste', e => {
-    const innerHTML = e.clipboardData.getData('text/html')
+  document.addEventListener('paste', (event) => {
+    const innerHTML = event.clipboardData.getData('text/html')
 
-    const code = e.clipboardData.getData('text/plain')
+    const code = event.clipboardData.getData('text/plain')
     const minIndent = getMinIndent(code)
 
     const snippetBgColor = getSnippetBgColor(innerHTML)
@@ -112,30 +126,22 @@
       vscode.postMessage({
         type: 'updateBgColor',
         data: {
-          bgColor: snippetBgColor
-        }
+          bgColor: snippetBgColor,
+        },
       })
       updateEnvironment(snippetBgColor)
     }
 
-    if (minIndent !== 0) {
-      snippetNode.innerHTML = stripInitialIndent(innerHTML, minIndent)
-    } else {
+    if (minIndent === 0) {
       snippetNode.innerHTML = innerHTML
+    } else {
+      snippetNode.innerHTML = stripInitialIndent(innerHTML, minIndent)
     }
 
     vscode.setState({ innerHTML })
   })
 
-  obturateur.addEventListener('click', () => {
-    if (target === 'container') {
-      shootAll() 
-    } else {
-      shootSnippet()
-    }
-  })
-
-  function shootAll() {
+  const shootAll = () => {
     const width = snippetContainerNode.offsetWidth * 2
     const height = snippetContainerNode.offsetHeight * 2
     const config = {
@@ -144,24 +150,24 @@
       style: {
         transform: 'scale(2)',
         'transform-origin': 'center',
-        background: getRgba(backgroundColor, transparentBackground)
-      }
+        background: getRgba(backgroundColor, transparentBackground),
+      },
     }
 
     // Hide resizer before capture
     snippetNode.style.resize = 'none'
     snippetContainerNode.style.resize = 'none'
 
-    domtoimage.toBlob(snippetContainerNode, config).then(blob => {
+    domtoimage.toBlob(snippetContainerNode, config).then((blob) => {
       snippetNode.style.resize = ''
       snippetContainerNode.style.resize = ''
-      serializeBlob(blob, serializedBlob => {
+      serializeBlob(blob, (serializedBlob) => {
         shoot(serializedBlob)
       })
     })
   }
 
-  function shootSnippet() {
+  const shootSnippet = () => {
     const width = snippetNode.offsetWidth * 2
     const height = snippetNode.offsetHeight * 2
     const config = {
@@ -171,51 +177,55 @@
         transform: 'scale(2)',
         'transform-origin': 'center',
         padding: 0,
-        background: 'none'
-      }
+        background: 'none',
+      },
     }
 
     // Hide resizer before capture
     snippetNode.style.resize = 'none'
     snippetContainerNode.style.resize = 'none'
 
-    domtoimage.toBlob(snippetContainerNode, config).then(blob => {
+    domtoimage.toBlob(snippetContainerNode, config).then((blob) => {
       snippetNode.style.resize = ''
       snippetContainerNode.style.resize = ''
-      serializeBlob(blob, serializedBlob => {
+      serializeBlob(blob, (serializedBlob) => {
         shoot(serializedBlob)
       })
     })
   }
 
+  obturateur.addEventListener('click', () => {
+    if (target === 'container') {
+      shootAll()
+    } else {
+      shootSnippet()
+    }
+  })
+
+
+  const buttonVivus = new Vivus('save', { duration: 40 })
   let isInAnimation = false
 
   obturateur.addEventListener('mouseover', () => {
     if (!isInAnimation) {
       isInAnimation = true
-
-      new Vivus(
-        'save',
-        {
-          duration: 40,
-          onReady: () => {
-            obturateur.className = 'obturateur filling'
-          }
-        },
-        () => {
+      obturateur.className = 'obturateur filling'
+      buttonVivus
+        .stop()
+        .reset()
+        .play(() => {
           setTimeout(() => {
             isInAnimation = false
             obturateur.className = 'obturateur'
           }, 700)
-        }
-      )
+        })
     }
   })
 
-  window.addEventListener('message', e => {
-    if (e) {
-      if (e.data.type === 'init') {
-        const { fontFamily, bgColor } = e.data
+  window.addEventListener('message', (event) => {
+    if (event) {
+      if (event.data.type === 'init') {
+        const { fontFamily, bgColor } = event.data
 
         const initialHtml = getInitialHtml(fontFamily)
         snippetNode.innerHTML = initialHtml
@@ -228,21 +238,20 @@
         } else {
           snippetContainerNode.style.background = 'none'
         }
-
-      } else if (e.data.type === 'update') {
+      } else if (event.data.type === 'update') {
         document.execCommand('paste')
-      } else if (e.data.type === 'restore') {
-        snippetNode.innerHTML = e.data.innerHTML
-        updateEnvironment(e.data.bgColor)
-      } else if (e.data.type === 'restoreBgColor') {
-        updateEnvironment(e.data.bgColor)
-      } else if (e.data.type === 'updateSettings') {
-        snippetNode.style.boxShadow = e.data.shadow
-        target = e.data.target
-        transparentBackground = e.data.transparentBackground
-        snippetContainerNode.style.backgroundColor = e.data.backgroundColor
-        backgroundColor = e.data.backgroundColor
-        if (e.data.ligature) {
+      } else if (event.data.type === 'restore') {
+        snippetNode.innerHTML = event.data.innerHTML
+        updateEnvironment(event.data.bgColor)
+      } else if (event.data.type === 'restoreBgColor') {
+        updateEnvironment(event.data.bgColor)
+      } else if (event.data.type === 'updateSettings') {
+        snippetNode.style.boxShadow = event.data.shadow
+        target = event.data.target
+        transparentBackground = event.data.transparentBackground
+        snippetContainerNode.style.backgroundColor = event.data.backgroundColor
+        backgroundColor = event.data.backgroundColor
+        if (event.data.ligature) {
           snippetNode.style.fontVariantLigatures = 'normal'
         } else {
           snippetNode.style.fontVariantLigatures = 'none'
@@ -250,13 +259,6 @@
       }
     }
   })
-})()
-
-function getRgba(hex, transparentBackground) {
-  const bigint = parseInt(hex.slice(1), 16);
-  const r = (bigint >> 16) & 255;
-  const g = (bigint >> 8) & 255;
-  const b = bigint & 255;
-  const a = transparentBackground ? 0 : 1
-  return `rgba(${r}, ${g}, ${b}, ${a})`
 }
+
+activate()
